abe818e07be6ba5fba8e3dc20c8e47958e45939c
Nick Malcolm <nick@malcolm.net.nz>
2016-06-06 17:37:56 +1200

    Rudimentary month position indicator

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -24,13 +24,46 @@ var template = Handlebars.compile(source);
 
 var region   = $("#events-region");
 
+var moveDateMarkerTo = function(string){
+  var datesEls = $("nav .dates"),
+    date = moment(string),
+    month = date.month() + 1,
+    dayOfMonth = date.date(),
+    positionMultiplier = null;
+
+  if (dayOfMonth <= 10) {
+    // Start of the month, but a little to the left for niceness
+    positionMultiplier = 0.1;
+  } else if (dayOfMonth <= 20) {
+    positionMultiplier = 0.5;
+  } else {
+    // End of the month, but a little to the right for niceness
+    positionMultiplier = 0.9;
+  }
+
+  $.each(datesEls, function(index, element){
+    var redLineEl = $(element).find(".red-line"),
+      newMonth = $("nav .dates").find("a[data-month='" + month + "']"),
+      // The month, shifted to the right depending on where in the month we are
+      position = newMonth.position().left + newMonth.width() * positionMultiplier;
+
+    $(redLineEl).animate({left: position});
+  });
+}
+
 $.getJSON('/events.json', function(data){
   data.sort(function(a, b) {
     return moment(a.date).isAfter( moment(b.date) );
   });
 
   $.each(data, function(index, item){
-    var html = template(item);
-    region.append(html);
+    var el = $(template(item));
+    el.appendTo(region);
+    $(el).waypoint({
+      handler: function(direction) {
+        moveDateMarkerTo(el.data("date"));
+      },
+      offset: '48px'
+    });
   });
 });
