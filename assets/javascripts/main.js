e9eb1118c4353b5fdc6878f24a9d68935be7167e
Nick Malcolm <nick@malcolm.net.nz>
2016-06-09 23:29:26 +1200

    Let's just turn off CloudFlare's rocket.

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,90 +1,88 @@
-Zepto(function($){
-  Handlebars.registerHelper('toParam', function(string) {
-    var string = Handlebars.escapeExpression(string);
-    return new Handlebars.SafeString(
-       string.replace(/[^0-9a-z\ \-]/gi, '').replace(/\s+/g, '-').toLowerCase()
-    );
-  });
-  Handlebars.registerHelper('strftime', function(string, format) {
-    var string = Handlebars.escapeExpression(string),
-        format = Handlebars.escapeExpression(format);
+Handlebars.registerHelper('toParam', function(string) {
+  var string = Handlebars.escapeExpression(string);
+  return new Handlebars.SafeString(
+     string.replace(/[^0-9a-z\ \-]/gi, '').replace(/\s+/g, '-').toLowerCase()
+  );
+});
+Handlebars.registerHelper('strftime', function(string, format) {
+  var string = Handlebars.escapeExpression(string),
+      format = Handlebars.escapeExpression(format);
 
-    return new Handlebars.SafeString(
-       moment(string).format(format)
-    );
-  });
-  Handlebars.registerHelper('bucketFromDate', function(string) {
-    var string = Handlebars.escapeExpression(string),
-        date = moment(string),
-        dayOfMonth = date.date();
-    if (dayOfMonth <= 10) {
-      return "Early";
-    } else if (dayOfMonth <= 20) {
-      return "Mid";
-    } else {
-      return "Late";
-    }
-  });
-
-  var moveDateMarkerTo = function(string){
-    var datesEls = $("nav .dates"),
+  return new Handlebars.SafeString(
+     moment(string).format(format)
+  );
+});
+Handlebars.registerHelper('bucketFromDate', function(string) {
+  var string = Handlebars.escapeExpression(string),
       date = moment(string),
-      month = date.month() + 1,
-      dayOfMonth = date.date(),
-      positionMultiplier = null;
+      dayOfMonth = date.date();
+  if (dayOfMonth <= 10) {
+    return "Early";
+  } else if (dayOfMonth <= 20) {
+    return "Mid";
+  } else {
+    return "Late";
+  }
+});
 
-    if (dayOfMonth <= 10) {
-      // Start of the month, but a little to the left for niceness
-      positionMultiplier = 0.1;
-    } else if (dayOfMonth <= 20) {
-      positionMultiplier = 0.5;
-    } else {
-      // End of the month, but a little to the right for niceness
-      positionMultiplier = 0.9;
-    }
+var moveDateMarkerTo = function(string){
+  var datesEls = $("nav .dates"),
+    date = moment(string),
+    month = date.month() + 1,
+    dayOfMonth = date.date(),
+    positionMultiplier = null;
 
-    $.each(datesEls, function(index, element){
-      var redLineEl = $(element).find(".red-line"),
-        newMonth = $(element).find("a[data-month='" + month + "']"),
-        // The month, shifted to the right depending on where in the month we are
-        position = newMonth.position().left + newMonth.width() * positionMultiplier;
-
-      $(redLineEl).animate({left: position});
-    });
+  if (dayOfMonth <= 10) {
+    // Start of the month, but a little to the left for niceness
+    positionMultiplier = 0.1;
+  } else if (dayOfMonth <= 20) {
+    positionMultiplier = 0.5;
+  } else {
+    // End of the month, but a little to the right for niceness
+    positionMultiplier = 0.9;
   }
 
-  var eventSource   = $("#event-template").html();
-  var eventTemplate = Handlebars.compile(eventSource);
-  var region   = $("#events-region");
+  $.each(datesEls, function(index, element){
+    var redLineEl = $(element).find(".red-line"),
+      newMonth = $(element).find("a[data-month='" + month + "']"),
+      // The month, shifted to the right depending on where in the month we are
+      position = newMonth.position().left + newMonth.width() * positionMultiplier;
 
-  $.getJSON('/events.json', function(data){
-    data.sort(function(a, b) {
-      return moment(a.date) - moment(b.date);
-    });
-
-    // Clear out the Enable JS message
-    region.html("");
-
-    $.each(data, function(index, item){
-
-      // Make this event drop an anchor if it's the first for its month
-      var month = moment(item.date).format("MMM").toLowerCase();
-      if($("#" + month).length == 0){
-        // The template will add an anchor
-        item.id = month;
-        // And let's activate the month in the nav bar
-        $("a[href='#" + month + "']").addClass("present");
-      }
-
-      var el = $(eventTemplate(item));
-      el.appendTo(region);
-
-      $(el).waypoint({
-        handler: function(direction) {
-          moveDateMarkerTo(el.data("date"));
-        },
-        offset: '48px'
-      });
-    });
+    $(redLineEl).animate({left: position});
   });
 }
+
+var eventSource   = $("#event-template").html();
+var eventTemplate = Handlebars.compile(eventSource);
+var region   = $("#events-region");
+
+$.getJSON('/events.json', function(data){
+  data.sort(function(a, b) {
+    return moment(a.date) - moment(b.date);
+  });
+
+  // Clear out the Enable JS message
+  region.html("");
+
+  $.each(data, function(index, item){
+
+    // Make this event drop an anchor if it's the first for its month
+    var month = moment(item.date).format("MMM").toLowerCase();
+    if($("#" + month).length == 0){
+      // The template will add an anchor
+      item.id = month;
+      // And let's activate the month in the nav bar
+      $("a[href='#" + month + "']").addClass("present");
+    }
+
+    var el = $(eventTemplate(item));
+    el.appendTo(region);
+
+    $(el).waypoint({
+      handler: function(direction) {
+        moveDateMarkerTo(el.data("date"));
+      },
+      offset: '48px'
+    });
+  });
+});
