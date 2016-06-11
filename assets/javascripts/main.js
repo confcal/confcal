ac25ccc68858c78cd0b71a76805a8815eee247d4
Nick Malcolm <nick@malcolm.net.nz>
2016-06-11 23:26:58 +1200

    Make it so that the list of events always starts at this month, and adjust the date marker appropriately.

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -25,9 +25,9 @@ Handlebars.registerHelper('bucketFromDate', function(string) {
   }
 });
 
-var moveDateMarkerTo = function(string){
+moveDateMarkerTo = function(string){
   var datesEls = $("nav .dates"),
-    date = moment(string),
+    date = moment(string, "YYYY-MM-DD"),
     month = date.month() + 1,
     dayOfMonth = date.date(),
     positionMultiplier = null;
@@ -49,6 +49,7 @@ var moveDateMarkerTo = function(string){
       position = newMonth.position().left + newMonth.width() * positionMultiplier;
 
     $(redLineEl).animate({left: position});
+    $(redLineEl).removeClass("hidden");
   });
 }
 
@@ -57,17 +58,38 @@ var eventTemplate = Handlebars.compile(eventSource);
 var region   = $("#events-region");
 
 $.getJSON('/events.json', function(data){
+
+  // Sort chronologically
   data.sort(function(a, b) {
-    return moment(a.date) - moment(b.date);
+    return moment(a.date, "YYYY-MM-DD") - moment(b.date, "YYYY-MM-DD");
+  });
+
+
+  // Reorder the array so that events start from the current month
+  var thisMonth = moment().month();
+
+  $.each(data, function(index, item){
+    // If we've reached this month (or later), this is where we want to split.
+    if (moment(item.date, "YYYY-MM-DD").month() >= thisMonth) {
+      // Split the array in two
+      var before = data.slice(0, index),
+        after = data.slice(index);
+      // And stich it together again, with this month at the front
+      data = after.concat(before);
+
+      // Returning false will break the $.each loop
+      return false;
+    }
   });
 
   // Clear out the Enable JS message
   region.html("");
 
+  // Create and append the event elements using handlebars
   $.each(data, function(index, item){
 
     // Make this event drop an anchor if it's the first for its month
-    var month = moment(item.date).format("MMM").toLowerCase();
+    var month = moment(item.date, "YYYY-MM-DD").format("MMM").toLowerCase();
     if($("#" + month).length == 0){
       // The template will add an anchor
       item.id = month;
@@ -78,6 +100,8 @@ $.getJSON('/events.json', function(data){
     var el = $(eventTemplate(item));
     el.appendTo(region);
 
+    // Add a waypoint so that the date nav scrolls to this event's month when
+    // you scroll past it
     $(el).waypoint({
       handler: function(direction) {
         moveDateMarkerTo(el.data("date"));
@@ -85,4 +109,19 @@ $.getJSON('/events.json', function(data){
       offset: '48px'
     });
   });
+
+  // If the page isn't scrolled already (which would trigger the dateMarker
+  // movement), then move the dateMarker to the date of the first event in the
+  // list (probably this month)
+  if ($(window).scrollTop() < 48) {
+    var firstEl = $(".event").first(),
+      firstDate = firstEl.data("date");
+
+    // Do it with a timeout, because otherwise the position is off for unknown
+    // reasons
+    setTimeout(function() {
+      moveDateMarkerTo(firstDate);
+    }, 30);
+  }
+
 });
